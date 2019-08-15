0f3f70638e156eef29802bea2446185f93ddbf21
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-08-15 11:14:05 -0400

    Integrated the /api/ URL

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ urlpatterns = [
     path("users/", include("flaim.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path("api/", include("flaim.database.urls"))
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:

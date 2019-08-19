775f9a803b40519ee9c6b2cb16e5643b7e8271e4
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-08-19 14:07:58 -0400

    Registered the models

--- flaim/database/admin.py
+++ flaim/database/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from flaim.database import models
 
 # Register your models here.
+admin.site.register(models.Product)
+admin.site.register(models.LoblawsProduct)
+admin.site.register(models.AmazonProduct)
+admin.site.register(models.WalmartProduct)
+admin.site.register(models.NutritionFacts)

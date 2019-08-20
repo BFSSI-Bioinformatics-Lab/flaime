6d2cdf9896425ae2907f0f8805934b04e9d2a2e3
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-08-20 10:40:38 -0400

    Initial commit

---
+++ flaim/product_browser/templatetags/product_browser_extras.py
@@ -0,0 +1,27 @@
+from django.template.defaulttags import register
+
+""" Note that these tags were registered in config.settings.base in the TEMPLATES section under libraries """
+
+
+@register.filter
+def format_dv(val):
+    # Since dv values are stored from 0 to 1 in the database, *100 and append "%" to make values more readable
+    if val is not None:
+        return f"{int(val * 100)} %"
+    else:
+        return ""
+
+
+@register.filter
+def format_nutrient_value(val):
+    # Since the data is stored strictly as g in the database; if the value is less than 1, convert to milligrams
+    if val is not None:
+        if val < 1:
+            return f"{int(val * 1000)} mg"
+        else:
+            if float(val).is_integer():
+                return f"{int(val)} g"
+            else:
+                return f"{float(val):1} g"
+    else:
+        return ""

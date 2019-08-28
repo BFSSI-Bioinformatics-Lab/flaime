0c9c7f811f0b2e6b9fcc9b45d2562b13ef62aa36
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-08-28 08:43:49 -0400

    New models and methods

--- flaim/database/models.py
+++ flaim/database/models.py
@@ -1,7 +1,33 @@
+from time import strftime
+from pathlib import Path
 from django.db import models
 from typing import Optional, Union
-
+from django.conf import settings
+from django.contrib.postgres.fields import JSONField
 from flaim.database.nutrient_coding import VALID_NUTRIENT_COLUMNS
+
+
+def upload_product_image(obj):
+    """ Sets the directory for an input ProductImage """
+    store = obj.product.store
+    date = strftime("%Y%m%d")
+    return f"{store}/{date}"
+
+
+def upload_fop_image(obj):
+    """
+    Note: obj must be instance of ProductImage model
+
+    Takes the parent product_image name e.g. /LOBLAWS/20190101/something.jpg and morphs it into
+    /LOBLAWS/20190101/something_fop.jpg
+
+    Reference for how ImageField attributes can be accessed:
+    https://docs.djangoproject.com/en/2.2/topics/files/
+    """
+    parent_path = Path(obj.product_image.image_path.name)  # e.g. /LOBLAWS/20190101/something.jpg
+    suffix = parent_path.suffix
+    fop_path = parent_path.with_suffix(f'_FOP{suffix}')
+    return fop_path
 
 
 class TimeStampedModel(models.Model):
@@ -198,11 +224,14 @@ class NutritionFacts(TimeStampedModel):
 # LOBLAWS MODELS
 
 class LoblawsProduct(TimeStampedModel):
+    """
+        Extension of the generic Product model to store Loblaws specific metadata
+    """
     product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name="loblaws_product")
     subcategory = models.CharField(max_length=100, blank=True, null=True)
     section = models.CharField(max_length=100, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    image_directory = models.CharField(max_length=500, blank=True, null=True)
+    image_directory = models.CharField(max_length=1000, blank=True, null=True)  # TODO: Consider implementing upload_to
     breadcrumbs = models.CharField(max_length=300, blank=True, null=True)
 
     def __str__(self):
@@ -216,7 +245,11 @@ class LoblawsProduct(TimeStampedModel):
 # WALMART MODELS
 
 class WalmartProduct(TimeStampedModel):
+    """
+        Extension of the generic Product model to store Walmart specific metadata
+    """
     product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name="walmart_product")
+    image_directory = models.CharField(max_length=500, blank=True, null=True)
 
     def __str__(self):
         return f"{self.product.product_code}: {self.product.name}"
@@ -229,6 +262,9 @@ class WalmartProduct(TimeStampedModel):
 # AMAZON MODELS
 
 class AmazonProduct(TimeStampedModel):
+    """
+        Extension of the generic Product model to store Amazon specific metadata
+    """
     product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name="amazon_product")
     bullet_points = models.TextField(null=True, blank=True)  # The Amazon description field
     manufacturer_reference = models.CharField(max_length=100, blank=True,
@@ -238,6 +274,7 @@ class AmazonProduct(TimeStampedModel):
     item_weight = models.CharField(max_length=30, blank=True, null=True)  # e.g. 1.58 kg
     parcel_dimensions = models.CharField(max_length=30, blank=True, null=True)  # e.g. 30 x 30 x 30 cm
     date_first_available = models.DateField(null=True, blank=True)  # from the Additional Information table
+    image_directory = models.CharField(max_length=1000, blank=True, null=True)
 
     def __str__(self):
         return f"{self.product.product_code}: {self.product.name}"
@@ -248,14 +285,23 @@ class AmazonProduct(TimeStampedModel):
 
 
 class AmazonSearchResult(TimeStampedModel):
-    """ Stores information on the search page that a particular product showed up on """
+    """
+        Stores information on the search page that a particular product showed up on
+    """
     product = models.ForeignKey(AmazonProduct, on_delete=models.CASCADE, related_name="amazon_search_result")
     search_string = models.CharField(max_length=200)  # Search string used to generate results
     page = models.IntegerField()  # Page that the product showed up on
     item_number = models.IntegerField()  # Of products shown on search page, tracks the index of the product (i.e. 2nd)
 
+    class Meta:
+        verbose_name = 'Amazon Search Result'
+        verbose_name_plural = 'Amazon Search Results'
+
 
 class AmazonProductReview(TimeStampedModel):
+    """
+        Model to store information on a single Amazon product review
+    """
     # One Amazon Product will have many reviews
     product = models.ForeignKey(AmazonProduct, on_delete=models.CASCADE, related_name="amazon_product_reviews")
     review_title = models.CharField(max_length=300)
@@ -264,24 +310,74 @@ class AmazonProductReview(TimeStampedModel):
     rating = models.FloatField()  # e.g. 3 out of 5 stars -> convert to float
     helpful = models.BooleanField()  # Flag for if the review was marked as helpful or not by Amazon
 
+    class Meta:
+        verbose_name = 'Amazon Product Review'
+        verbose_name_plural = 'Amazon Product Reviews'
+
 
 # IMAGE CLASSIFICATION
+class ProductImage(TimeStampedModel):
+    """
+        Model to store relationship between a Product and a path to a product image
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_image")
+    image_path = models.ImageField(upload_to=upload_product_image, unique=True, max_length=1000)
+    image_number = models.IntegerField(blank=True, null=True)  # Order of the image in a set for a product
 
-class ProductImageClassification(TimeStampedModel):
-    """ Stores basic classification information on a product image """
+    def __str__(self):
+        return f"{self.product}: {Path(self.image_path.url).name}"
+
+    class Meta:
+        verbose_name = 'Product Image'
+        verbose_name_plural = 'Product Images'
+
+
+class NutritionLabelClassification(TimeStampedModel):
+    """
+        Stores output from the nutrition label classifier for a ProductImage
+    """
     # Required fields
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="image_classification")
-    image_path = models.CharField(max_length=1000)
-    image_number = models.IntegerField()  # Order of the image in a set for a product
+    product_image = models.ForeignKey(ProductImage, on_delete=models.CASCADE, related_name="image_classification")
 
     # Classification can be populated after the fact as it can take time to calculate
     classification_choices = (
-        ('NUTRITION', 'N'),
-        ('INGREDIENTS', 'I'),
-        ('OTHER', 'O'),
+        ('N', 'NUTRITION'),
+        ('I', 'INGREDIENTS'),
+        ('O', 'OTHER'),
     )
     classification = models.CharField(choices=classification_choices, max_length=15, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Image Classification'
         verbose_name_plural = 'Image Classifications'
+
+
+class FrontOfPackLabel(TimeStampedModel):
+    """
+    Stores output from the FrontOfPackLabel classifier
+    """
+    product_image = models.OneToOneField(ProductImage, on_delete=models.CASCADE, related_name="fop_label")
+
+    # Results from the classifier should be available in a simple JSON object
+    classifier_result_json = JSONField(blank=True, null=True)
+
+    # Boolean to represent if a FOP label is present or not
+    label_detected = models.BooleanField()
+
+    # TODO: Figure out what kind of classifications we can nail down with the model
+    label_type_choices = (
+        ('FC', 'FOOD_COLOURING'),
+    )
+    label_type = models.CharField(max_length=30, choices=label_type_choices, null=True, blank=True)
+    classified_image_path = models.ImageField(upload_to=upload_fop_image, max_length=1000)
+
+    def __str__(self):
+        return f"{self.product_image.product.name} - FOP Present: {self.label_detected}"
+
+    @property
+    def classified_image_filename(self):
+        return Path(self.classified_image_path.path).name
+
+    class Meta:
+        verbose_name = 'FOP Label'
+        verbose_name_plural = 'FOP Labels'

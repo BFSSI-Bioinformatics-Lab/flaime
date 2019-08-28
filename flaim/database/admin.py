d8dea81bbd4084938a17732ade911a19c98bdc2d
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-08-28 08:43:33 -0400

    Registered new models

--- flaim/database/admin.py
+++ flaim/database/admin.py
@@ -3,7 +3,16 @@ from flaim.database import models
 
 # Register your models here.
 admin.site.register(models.Product)
-admin.site.register(models.LoblawsProduct)
-admin.site.register(models.AmazonProduct)
-admin.site.register(models.WalmartProduct)
 admin.site.register(models.NutritionFacts)
+admin.site.register(models.ProductImage)
+
+admin.site.register(models.LoblawsProduct)
+
+admin.site.register(models.WalmartProduct)
+
+admin.site.register(models.AmazonProduct)
+admin.site.register(models.AmazonProductReview)
+admin.site.register(models.AmazonSearchResult)
+
+admin.site.register(models.FrontOfPackLabel)
+admin.site.register(models.NutritionLabelClassification)

2e7bcf085fffd309f57b3a2355e8329e322174e3
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-08-28 08:46:12 -0400

    Added several filter backends to ProductViewSet Made ViewSets for FrontOfPackLabel and ProductImage

--- flaim/database/views.py
+++ flaim/database/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+import logging
+import rest_framework_datatables
+
 from django.contrib.auth.models import Group
-from rest_framework import viewsets
+
+from rest_framework import viewsets, filters
+from django_filters import rest_framework as df_filters
 
 from flaim.database import models
 from flaim.database import serializers
@@ -8,11 +12,22 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+logger = logging.getLogger(__name__)
+
 
 # Create your views here.
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = models.Product.objects.all().order_by('-created')
     serializer_class = serializers.ProductSerializer
+
+    # Stacking a lot of df_filters here; this might be an issue but this is purely speculation
+    filter_backends = [df_filters.DjangoFilterBackend,
+                       rest_framework_datatables.filters.DatatablesFilterBackend,
+                       filters.SearchFilter
+                       ]
+    filterset_fields = ('name', 'store', 'product_code', 'brand',
+                        'loblaws_product__section', 'loblaws_product__subcategory')
+    search_fields = ['name', 'store', 'brand']
 
 
 class NutritionFactsViewSet(viewsets.ModelViewSet):
@@ -33,6 +48,19 @@ class WalmartProductViewSet(viewsets.ModelViewSet):
 class AmazonProductViewSet(viewsets.ModelViewSet):
     queryset = models.AmazonProduct.objects.all().order_by('-created')
     serializer_class = serializers.AmazonProductSerializer
+
+
+class ProductImageViewSet(viewsets.ModelViewSet):
+    queryset = models.ProductImage.objects.all().order_by('-created')
+    serializer_class = serializers.ProductImageSerializer
+
+
+class FrontOfPackLabelViewSet(viewsets.ModelViewSet):
+    queryset = models.FrontOfPackLabel.objects.all().order_by('-created')
+    serializer_class = serializers.FrontOfPackLabelSerializer
+
+    filter_backends = [df_filters.DjangoFilterBackend]
+    filterset_fields = ('id', 'product_image', 'label_detected',)
 
 
 class UserViewSet(viewsets.ModelViewSet):

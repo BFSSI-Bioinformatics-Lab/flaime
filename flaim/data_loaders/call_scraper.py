95e26e2c153de81999e8cfd246df5bdc608be098
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-08-28 08:46:28 -0400

    Script can now be called directly

--- flaim/data_loaders/call_scraper.py
+++ flaim/data_loaders/call_scraper.py
@@ -1,3 +1,10 @@
+import os
+import django
+
+# Need to do this in order to access the flaim database models
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+django.setup()
+
 from pathlib import Path
 from django.conf import settings
 from flaim.data_loaders.loblaws_scraper import run_loblaws_scraper
@@ -9,9 +16,7 @@ def enqueue_loblaws_scraper(url: str, outdir: Path, filter_keyword: str):
 
 
 if __name__ == "__main__":
-
-    loblaws_outdir = Path(settings.MEDIA_ROOT) / 'LOBLAWS_PRODUCTS'
-    loblaws_outdir.mkdir(exist_ok=True, parents=True)
+    loblaws_outdir = Path(settings.MEDIA_ROOT)
     enqueue_loblaws_scraper(url='https://www.loblaws.ca/Food/Pantry/Breakfast/c/LSL001008004000?navid=Breakfast',
                             outdir=loblaws_outdir,
                             filter_keyword='CEREAL')

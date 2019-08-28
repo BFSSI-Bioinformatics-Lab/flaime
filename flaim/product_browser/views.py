6ea47dc3e77be0bbb9835883908ebd6e0adb1c48
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-08-28 08:43:16 -0400

    Support for FOP tab

--- flaim/product_browser/views.py
+++ flaim/product_browser/views.py
@@ -2,7 +2,7 @@ import logging
 from pathlib import Path
 from django.conf import settings
 from django.views.generic import TemplateView, DetailView
-from flaim.database.models import Product
+from flaim.database.models import Product, ProductImage, FrontOfPackLabel
 
 logger = logging.getLogger(__name__)
 
@@ -20,10 +20,19 @@ class ProductView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        # Get image paths for the product
+        # Get image paths for the product. This is pretty gross and should be refactored. TODO: Make this not terrible
         images = list(Path(context['product'].loblaws_product.image_directory).glob("*"))
+        images = [x for x in images if x.is_file()]
         images = [str(x).replace(settings.MEDIA_ROOT, settings.MEDIA_URL[:-1]) for x in images]
         context['product_images'] = images
+
+        # Images
+        product_image_objects = ProductImage.objects.filter(product=context['product'])
+        context['product_image_objects'] = product_image_objects
+
+        # FOP
+        fop_objects = FrontOfPackLabel.objects.filter(product_image__in=product_image_objects)
+        context['fop_objects'] = fop_objects
 
         logger.debug(context)
         logger.debug(context['product_images'])

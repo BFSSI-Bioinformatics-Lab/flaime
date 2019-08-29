b4031362c3ce156d59f44cf625e5bdb6e055e981
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-08-29 08:55:05 -0400

    Now other computers can access the portal on the network

--- config/settings/local.py
+++ config/settings/local.py
@@ -8,7 +8,7 @@ DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "*"]  # '*' is super insecure -_-
 
 # CACHES
 # ------------------------------------------------------------------------------

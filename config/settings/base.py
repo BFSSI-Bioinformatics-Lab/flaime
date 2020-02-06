ee6bc62daa7abbb921159e4417195713feb5ce08
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-02-06 09:37:18 -0500

    Add simple-history, TIME_ZONE=Canada/Eastern

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,7 +25,7 @@ DEBUG = env.bool("DJANGO_DEBUG", False)
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
 # In Windows, this must be set to your system time zone.
-TIME_ZONE = "UTC"
+TIME_ZONE = "Canada/Eastern"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = "en-us"
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
@@ -81,6 +81,7 @@ THIRD_PARTY_APPS = [
     "rest_framework_datatables",
     "django_rq",
     "django_filters",  # Filter library for DRF
+    "simple_history"
 ]
 
 LOCAL_APPS = [
@@ -145,6 +146,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'simple_history.middleware.HistoryRequestMiddleware',
 ]
 
 # STATIC
@@ -293,7 +295,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework_datatables.pagination.DatatablesPageNumberPagination',
     'PAGE_SIZE': 15,
 }
-
 
 RQ_QUEUES = {
     'default': {

1c739e6f582e9d88f41873a68e4ea53d2f0b09d9
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-02-07 11:37:18 -0500

    Initial commit

---
+++ flaim/data_loaders/loblaws/load_images_to_db.py
@@ -0,0 +1,35 @@
+import os
+import django
+
+# Need to do this in order to access the flaim database models
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+django.setup()
+
+from pathlib import Path
+from flaim.database.models import ProductImage, Product, LoblawsProduct
+from django.conf import settings
+from django.db import IntegrityError
+
+if __name__ == "__main__":
+    image_dirs = list(Path("/home/forest/PycharmProjects/flaim/flaim/media/LOBLAWS/20200206").glob('*'))
+    for d in image_dirs:
+        product_code = d.name
+        product = Product.objects.get(product_code=product_code)
+        loblaws_product = LoblawsProduct.objects.get(product__product_code=product_code)
+
+        # Update image directory
+        if loblaws_product.image_directory is None:
+            loblaws_product.image_directory = str(d)
+            loblaws_product.save()
+
+        # Strip out MEDIA_ROOT for paths to behave properly with image field in ProductImage
+        images = [str(x).replace(settings.MEDIA_ROOT + "/", "") for x in list(d.glob('*')) if x.is_file()]
+        for i in images:
+            try:
+                ProductImage.objects.create(product=product,
+                                            image_path=i)
+            # Skip if the file path already exists
+            except IntegrityError as e:
+                pass
+
+        print(f'Done with {product_code}')

3f205842b54fb980ce5a9a02ed6d49afdc9946d5
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-02-12 13:06:26 -0500

    Remove old code

--- flaim/product_search/views.py
+++ flaim/product_search/views.py
@@ -11,13 +11,4 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        # TODO: Currently dynamically generates a list; this is very inefficient, but accurate.
-        #  Better to keep a semi-regularly updated hardcoded list somewhere.
-        brand_list = models.Product.objects.all()
-        brand_list = list(set([x.brand for x in brand_list if x.brand is not None]))
-        context['brand_json'] = json.dumps(brand_list)
-
-        context['store_list'] = json.dumps(['LOBLAWS', 'WALMART', 'AMAZON'])
-
         return context

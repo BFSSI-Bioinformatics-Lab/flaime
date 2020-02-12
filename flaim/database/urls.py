28923800349019871eb6948a43e6fd7bceacf713
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-02-12 13:07:37 -0500

    Add AdvancedProductViewSet

--- flaim/database/urls.py
+++ flaim/database/urls.py
@@ -4,10 +4,13 @@ from flaim.database import views
 
 router = routers.DefaultRouter()
 router.register(r'products', views.ProductViewSet)
-router.register(r'nutrition_facts', views.NutritionFactsViewSet)
 router.register(r'loblaws_products', views.LoblawsProductViewSet)
 router.register(r'walmart_products', views.WalmartProductViewSet)
 router.register(r'amazon_products', views.AmazonProductViewSet)
+router.register(r'nutrition_facts', views.NutritionFactsViewSet)
+
+# Had to manually set the basename here because otherwise it tries to use the queryset as the basename
+router.register(r'advanced_product', views.AdvancedProductViewSet, basename='advanced_product')
 router.register(r'product_images', views.ProductImageViewSet)
 router.register(r'front_of_pack', views.FrontOfPackLabelViewSet)
 router.register(r'users', views.UserViewSet)

99d520bebbf7025f96b3f7be20bb01a712bc828d
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-02-14 11:41:45 -0500

    Add context for valid DV nutrient list

--- flaim/product_search/views.py
+++ flaim/product_search/views.py
@@ -2,7 +2,7 @@ import json
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from flaim.database import models
+from flaim.database.nutrient_coding import VALID_NUTRIENT_DICT
 
 
 # Create your views here.
@@ -11,4 +11,8 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        # Used to populate the select HTML elements for DV filtering
+        context['available_nutrients_dv'] = {k: v for k, v in VALID_NUTRIENT_DICT.items() if '_dv' in k}
+
         return context

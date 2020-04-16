3e315580bc2d327fccc8d65d3d5e6bb9c4921f00
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-04-16 09:17:38 -0400

    Add check for expected keys Add minor data cleaning steps

--- flaim/data_loaders/walmart/load_json_to_db.py
+++ flaim/data_loaders/walmart/load_json_to_db.py
@@ -1,19 +1,19 @@
 import os
 import json
+import django
 from pathlib import Path
 from django.conf import settings
 from django.db import IntegrityError
 
-import django
-
-# Need to do this in order to access the flaim database models
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 django.setup()
 
-from flaim.database.models import Product, WalmartProduct, NutritionFacts, ProductImage
+from flaim.database.models import Product, WalmartProduct, NutritionFacts, ProductImage, ScrapeBatch
 
-# For Django historical model
 CHANGE_REASON = 'New Walmart Scrape Batch'
+POTENTIAL_KEYS = {'ingredients_txt', 'url', 'nutrition', 'SKU', 'created_date', 'nielsen_product', 'product_code',
+                  'breadcrumbs', 'bullets', 'size', 'nft_american', 'website', 'images', 'product_name', 'UPC', 'Brand',
+                  'nft_present', 'nielsen_upc', 'price', 'long_desc', 'Lifestyle & Dietary Need'}
 
 
 def read_json(json_file):
@@ -23,21 +23,45 @@ def read_json(json_file):
 
 
 if __name__ == "__main__":
-    f = Path("/home/forest/Documents/FLAIME/walmart_data/subset_test.json")
+    f = Path("/home/forest/Documents/FLAIME/walmart_data/2020-04-07.json")
     image_dir = Path("/home/forest/PycharmProjects/flaim/flaim/media/WALMART/")
     j = read_json(str(f))
 
-    # Create scrape batch
+    # Cleanup
     Product.objects.filter(store='WALMART').delete()
 
+    # Create scrape batch
+    # All Walmart products in the DB
+    existing_products = [x.product.product_code for x in WalmartProduct.objects.all()]
+    product_codes = [x['product_code'] for x in j]
+    missing_products = len(list(set(existing_products) - set(product_codes)))
+    new_products = len([x for x in product_codes if x not in existing_products])
+    total_products = len(j)
+
+    scrape = ScrapeBatch.objects.create(
+        missing_products=missing_products,
+        new_products=new_products,
+        total_products=total_products,
+    )
+
     for p in j:
+
+        # Make sure all of the expected keys are populated at least with None.
+        for k in POTENTIAL_KEYS:
+            if k not in p:
+                p[k] = None
+
         product, created = Product.objects.get_or_create(product_code=p['product_code'])
 
         # Product fields
         product.name = p['product_name']
         product.store = 'WALMART'
-        product.upc_code = p['UPC']
+
+        # TODO: Make sure the UPC code is just the first entry
+        first_upc_code = p['UPC'].split(',')[0]
+        product.upc_code = first_upc_code
         product.url = p['url']
+
         product.brand = p['Brand']
 
         if p['price'] == 'price unavailable':
@@ -77,6 +101,12 @@ if __name__ == "__main__":
                 nutrition_dict[key] = 0
             if val == 'conflict':
                 nutrition_dict[key] = None
+            # TODO: This is a hack to convert any 'o' values to numeric 0. This is an OCR error.
+            if '_dv' in key and val is not None:
+                if type(val) == str and val.lower() == 'o':
+                    nutrition_dict[key] = 0
+                elif type(val) == str:
+                    nutrition_dict[key] = None
 
         # Need to iterate through the nutrition dict and convert mg to grams, and dv/100
         for key, val in nutrition_dict.items():
@@ -91,7 +121,13 @@ if __name__ == "__main__":
 
         nutrition, c = NutritionFacts.objects.get_or_create(product=product)
         nutrition.ingredients = p['ingredients_txt']
-        nutrition.total_size = p['size']
+
+        # TODO: There's a bug where the size value can just be a crazy string. Here's a temporary hack to get around it.
+        if len(p['size']) < 100:
+            nutrition.total_size = p['size']
+        else:
+            nutrition.total_size = None
+
         if not c:
             nutrition.changeReason = CHANGE_REASON
 

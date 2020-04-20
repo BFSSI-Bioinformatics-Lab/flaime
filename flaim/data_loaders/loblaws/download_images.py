bb7941a5b91ac1bacd4f9edbce18cd7905abd38e
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-04-20 15:43:27 -0400

    Refactor to use JSON files instead of DB entries to retrieve images

--- flaim/data_loaders/loblaws/download_images.py
+++ flaim/data_loaders/loblaws/download_images.py
@@ -1,24 +1,45 @@
 import os
+import json
 import django
 import requests
 import time
-
 
 # Need to do this in order to access the flaim database models
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 django.setup()
 
 from pathlib import Path
-from datetime import datetime
-from flaim.database.models import LoblawsProduct, ProductImage
+from flaim.database.models import LoblawsProduct, ProductImage, ScrapeBatch
 from django.conf import settings
 
-# Output directory should represent day script was run
-OUTDIR = Path(settings.MEDIA_ROOT) / 'LOBLAWS' / datetime.today().strftime('%Y%m%d')
+# Output directory should represent day scrape was collected
+SCRAPE_DATE = '2020-04-17'
+OUTDIR = Path(settings.MEDIA_ROOT) / 'LOBLAWS' / SCRAPE_DATE
+DATADIR = Path(f"/home/forest/Documents/FLAIME/loblaws_data/product_data_{SCRAPE_DATE}")  # Product data
 
 
-def get_image_urls_from_product(product: LoblawsProduct) -> [str]:
-    images = product.get_large_image_urls()
+def safe_run(func):
+    """ Decorator to run a method wrapped in a try/except -> returns None upon exception """
+
+    def func_wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            return None
+
+    return func_wrapper
+
+
+def read_json(json_file):
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    return data
+
+
+@safe_run
+def get_large_image_urls(api_data):
+    """ This is typically the image data we want to retrieve per product """
+    images = [x['largeUrl'] for x in api_data['imageAssets']]
     return images
 
 
@@ -28,18 +49,27 @@ def download_image(url: str, outfile: Path) -> bool:
         handler.write(img_data)
     if outfile.exists():
         return True
+    print(f'URL {url} failed')
     return False
 
 
-if __name__ == "__main__":
-    products = LoblawsProduct.objects.filter(api_data__isnull=False)
-
-    for p in products:
-        # Check if we already have images in the DB
-        images = ProductImage.objects.filter(product=p.product)
-        if len(images) > 0:
+def download_all_images():
+    json_files = list(DATADIR.glob("*.json"))
+    for j in json_files:
+        print(f'Retrieving images for {j.name}')
+        try:
+            data = read_json(j)
+        except json.decoder.JSONDecodeError as e:
+            print(f'Skipping product JSON {j} due to exception:\n{e}')
             continue
-        image_urls = get_image_urls_from_product(p)
+
+        try:
+            p = LoblawsProduct.objects.get(product__product_code=str(j.with_suffix("").name))
+        except LoblawsProduct.DoesNotExist as e:
+            print(f'Corresponding database object does not exist for {j.with_suffix("").name}')
+            continue
+
+        image_urls = get_large_image_urls(data)
         if image_urls is None:
             continue
         download_dir = OUTDIR / p.product.product_code
@@ -48,9 +78,16 @@ if __name__ == "__main__":
 
         for i in image_urls:
             outfile = download_dir / Path(i).name
-            if outfile.exists():
+            try:
+                if outfile.exists():
+                    continue
+            except TypeError as e:
+                print(e)
                 continue
             status = download_image(url=i, outfile=outfile)
-        print(f'{status}\t{p.product.product_code}')
+        print(f'Images Retrieved: {status}\t{p.product.product_code}')
         time.sleep(1.5)
 
+
+if __name__ == "__main__":
+    download_all_images()

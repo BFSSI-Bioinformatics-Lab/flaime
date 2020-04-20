42d3ca0465e1fdf29f22698ef33fc7f07a823021
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-04-20 15:43:55 -0400

    Refactor

--- flaim/data_loaders/loblaws/load_images_to_db.py
+++ flaim/data_loaders/loblaws/load_images_to_db.py
@@ -1,29 +1,27 @@
 import os
+from pathlib import Path
+from django.conf import settings
+from django.utils.dateparse import parse_date
+from django.db import IntegrityError
+
 import django
 
-# Need to do this in order to access the flaim database models
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 django.setup()
 
-from pathlib import Path
-from flaim.database.models import ProductImage, Product, LoblawsProduct
-from django.conf import settings
-from django.db import IntegrityError
+from flaim.database.models import Product, ProductImage
 
-if __name__ == "__main__":
-    image_dirs = list(Path("/home/forest/PycharmProjects/flaim/flaim/media/LOBLAWS/20200214").glob('*'))
+SCRAPE_DATE = parse_date('2020-04-17')  # YYYY-MM-DD
+IMAGE_DIRS = list(Path(f"/home/forest/PycharmProjects/flaim/flaim/media/LOBLAWS/{str(SCRAPE_DATE)}").glob('*'))
+
+
+def load_images(image_dirs: list):
     for d in image_dirs:
         product_code = d.name
         try:
             product = Product.objects.get(product_code=product_code)
         except:
             continue
-        loblaws_product = LoblawsProduct.objects.get(product__product_code=product_code)
-
-        # Update image directory
-        if loblaws_product.image_directory is None:
-            loblaws_product.image_directory = str(d)
-            loblaws_product.save()
 
         # Strip out MEDIA_ROOT for paths to behave properly with image field in ProductImage
         images = [str(x).replace(settings.MEDIA_ROOT + "/", "") for x in list(d.glob('*')) if x.is_file()]
@@ -36,3 +34,7 @@ if __name__ == "__main__":
                 pass
 
         print(f'Done with {product_code}')
+
+
+if __name__ == "__main__":
+    load_images(IMAGE_DIRS)

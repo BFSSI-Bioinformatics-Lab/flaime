7fa31588877712e4ad697860f5980ab01477e7c6
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-06-12 14:38:19 -0400

    Batch testing

--- flaim/batch_browser/views.py
+++ flaim/batch_browser/views.py
@@ -1,9 +1,15 @@
 import logging
-from django.views.generic import TemplateView
+from flaim.database import models
+from django.views.generic import ListView
 
 logger = logging.getLogger(__name__)
 
 
-# Create your views here.
-class IndexView(TemplateView):
+class IndexView(ListView):
+    model = models.ScrapeBatch
     template_name = 'batch_browser/index.html'
+    context_object_name = 'batches'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context

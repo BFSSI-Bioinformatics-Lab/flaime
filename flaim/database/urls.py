b593b4502781249b6d0263683f1b24978e0af449
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-06-16 14:01:34 -0400

    Add basename to RecentProductViewSet router

--- flaim/database/urls.py
+++ flaim/database/urls.py
@@ -4,7 +4,7 @@ from flaim.database.api import views
 
 router = routers.DefaultRouter()
 router.register(r'products', views.ProductViewSet)
-router.register(r'recent_products', views.RecentProductViewSet)
+router.register(r'recent_products', views.RecentProductViewSet, basename='recent_products')
 router.register(r'loblaws_products', views.LoblawsProductViewSet)
 router.register(r'walmart_products', views.WalmartProductViewSet)
 router.register(r'amazon_products', views.AmazonProductViewSet)

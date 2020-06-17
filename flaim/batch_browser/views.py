cfd6e45a3d180c0bf97f83001227a9424cf9e372
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-06-17 11:16:41 -0400

    Add BatchView

--- flaim/batch_browser/views.py
+++ flaim/batch_browser/views.py
@@ -1,6 +1,6 @@
 import logging
 from flaim.database import models
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 logger = logging.getLogger(__name__)
 
@@ -12,4 +12,15 @@ class IndexView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        return context
+
+
+class BatchView(DetailView):
+    model = models.ScrapeBatch
+    template_name = 'batch_browser/batch_detail.html'
+    context_object_name = 'batch'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
         return context

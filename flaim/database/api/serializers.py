6700abb5396371444d8ac01082d74601e0c3d76d
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-06-17 11:51:56 -0400

    test

--- flaim/database/api/serializers.py
+++ flaim/database/api/serializers.py
@@ -85,7 +85,7 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer, EagerLoadingMixi
     loblaws_product = LoblawsProductSerializer()
     walmart_product = WalmartProductSerializer()
     batch = ScrapeBatchSerializer()
-    url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk')
+    url = serializers.HyperlinkedIdentityField(view_name='database:product-detail', lookup_field='id')
 
     class Meta:
         model = models.Product

ee4fee85364fed9ca37a9a6b0bb951f13a985260
brian <bfssi.brian.carignan@gmail.com>
2020-06-22 14:05:44 -0400

    added sample plot, switched to plotly v4 api for creating div

--- flaim/visualizer/views.py
+++ flaim/visualizer/views.py
@@ -2,8 +2,8 @@ from django.views.generic import ListView, View
 from flaim.database import models, nutrient_coding
 
 import pandas as pd
-from plotly.offline import plot
-from plotly.graph_objs import Scatter
+import plotly.graph_objects as go
+from plotly.io import to_html
 import json
 
 from django.core import serializers
@@ -22,22 +22,31 @@ from flaim.database.api.serializers import ProductSerializer
 
 # Create your views here.
 class IndexView(ListView):
-    model = models.NutritionFacts
+    model = models.Product
+    nutrition_facts = models.NutritionFacts
     template_name = 'visualizer/index.html'
     context_object_name = 'products'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        df = pd.DataFrame(list(self.model.objects.filter(product__most_recent=True).prefetch_related('product').values()))
-        print(df.columns)
+        df = pd.DataFrame(list(self.nutrition_facts.objects.filter(product__most_recent=True).values()))
+        # df1.to_csv('nutrition_facts.csv', index=False)
+        # df2 = pd.DataFrame(list(self.model.objects.filter(most_recent=True).values()))
+        # df2.to_csv('products.csv', index=False)
+        # print(df.columns)
 
-        x_data = [0, 1, 2, 3]
-        y_data = [x ** 2 for x in x_data]
-        plot_div = plot([Scatter(x=x_data, y=y_data,
-                                 mode='lines', name='test',
-                                 opacity=0.8, marker_color='green')],
-                        output_type='div', include_plotlyjs=True)
+        fig = go.Figure()
+        fig.add_trace(go.Box(x=df['sodium_dv'].dropna(), name='Sodium'))
+        fig.add_trace(go.Box(x=df['totalfat_dv'].dropna(), name='Total Fat'))
+        fig.add_trace(go.Box(x=df['sugar'].dropna() / 100, name='Sugar'))
+
+        fig.update_layout(barmode='stack')
+        fig.update_traces(opacity=0.75)
+
+        fig['layout']['xaxis'].update(range=[0, 1])
+
+        plot_div = to_html(fig, include_plotlyjs=False, full_html=False)
         context['plot_div'] = plot_div
         return context
 

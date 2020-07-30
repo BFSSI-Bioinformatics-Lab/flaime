1d5ef664d819252fab858da00aadfc532388e5e5
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-07-30 13:45:35 -0400

    add reports and data apps

--- config/settings/base.py
+++ config/settings/base.py
@@ -94,6 +94,8 @@ LOCAL_APPS = [
     'flaim.product_browser',
     'flaim.product_search',
     'flaim.batch_browser',
+    'flaim.reports',
+    'flaim.data',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS

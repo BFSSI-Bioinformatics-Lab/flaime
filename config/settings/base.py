89e2db8d7b2c2eb0d0ea82d776fdfffce1845858
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-08-13 14:21:58 -0400

    add classifiers to installed apps

--- config/settings/base.py
+++ config/settings/base.py
@@ -97,6 +97,7 @@ LOCAL_APPS = [
     'flaim.batch_browser',
     'flaim.reports',
     'flaim.data',
+    'flaim.classifiers',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS

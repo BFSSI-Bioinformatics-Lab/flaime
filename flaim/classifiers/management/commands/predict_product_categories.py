adfa82d87acc72ba856f51deadbab106ebc42648
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-08-14 14:54:59 -0400

    better path for pickled model can now decide if you want to update all products or just most_recent=True rename some things

--- flaim/classifiers/management/commands/predict_product_categories.py
+++ flaim/classifiers/management/commands/predict_product_categories.py
@@ -1,19 +1,13 @@
-import json
+import pandas as pd
 from pathlib import Path
-from django.utils.dateparse import parse_date
-from django.utils import timezone
-from django.conf import settings
-from django.db import IntegrityError
 from django.core.management.base import BaseCommand
 from flaim.database.models import Product, PredictedCategory
-from tqdm import tqdm
-import pandas as pd
-from flaim.classifiers.category_prediction import Predictor
-from flaim.classifiers.preprocessing import FLAIME
+from flaim.classifiers.category_prediction import CategoryPredictor
+from flaim.classifiers.category_preprocessing import FLAIME
 from flaim.database import models
+from tqdm import tqdm
 
-# TODO: set this in .env and read it from settings
-CATEGORY_PREDICTOR_MODEL = Path("/home/forest/PycharmProjects/flaim/flaim/classifiers/data/model_int_test.pkl")
+CATEGORY_PREDICTOR_MODEL = Path(__file__).parents[2] / 'data' / 'category_predictor.pkl'
 
 
 class Command(BaseCommand):
@@ -23,27 +17,30 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--all_products', action='store_true',
-                            help='Call this flag in order to predict categories for ALL '
-                                 'products in the database rather than only those with '
-                                 'most_recent=True')
+                            help='Call this flag in order to predict categories for ALL products in the database rather'
+                                 ' than only those with most_recent=True.')
 
     def handle(self, *args, **options):
         if options['all_products']:
-            self.stdout.write(self.style.SUCCESS(f'Predicting categories for ALL products in database '
-                                                 f'(actually this does not work yet, sorry!)'))
+            self.stdout.write(self.style.SUCCESS(f'Predicting categories for ALL products in database'))
+            most_recent_bool = False
         else:
             self.stdout.write(self.style.SUCCESS(f'Predicting categories for recent products in database'))
+            most_recent_bool = True
 
-        predictor = Predictor(CATEGORY_PREDICTOR_MODEL)
-        data = FLAIME(models.Product, models.NutritionFacts)
+        predictor = CategoryPredictor(CATEGORY_PREDICTOR_MODEL)
+
+        self.stdout.write(self.style.SUCCESS(f'Detected category prediction model version {predictor.model_version}'))
+
+        data = FLAIME(models.Product, models.NutritionFacts, most_recent_bool)
         predictions = predictor.predict(data)
 
         df = pd.concat([data.product_ids, data.names, predictions], axis=1)
-        for i, row in df.iterrows():
+        self.stdout.write(self.style.SUCCESS(f"Found {len(df)} products to update"))
+        for i, row in tqdm(df.iterrows(), desc="Predicting categories"):
             predicted_category = PredictedCategory.objects.create(predicted_category=row['Pred 1'],
                                                                   confidence=row['Conf 1'],
-                                                                  model_verison="dev-test")
+                                                                  model_verison=predictor.model_version)
             o = Product.objects.get(id=row['id'])
             o.predicted_category = predicted_category
             o.save()
-            self.stdout.write(self.style.SUCCESS(f"Set predicted category for {o} to {predicted_category}"))

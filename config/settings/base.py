ef4206e59351a9797b2c3bef1fa4adada39eb1be
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-08-18 10:48:27 -0400

    add product_curator app

--- config/settings/base.py
+++ config/settings/base.py
@@ -98,6 +98,7 @@ LOCAL_APPS = [
     'flaim.reports',
     'flaim.data',
     'flaim.classifiers',
+    'flaim.product_curator',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS

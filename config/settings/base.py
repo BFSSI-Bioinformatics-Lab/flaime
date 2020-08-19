edace25b881d11a968d2a0ff97f41668cfd28004
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-08-19 12:07:44 -0400

    Refactor to use npm

--- config/settings/base.py
+++ config/settings/base.py
@@ -164,7 +164,8 @@ STATIC_ROOT = str(ROOT_DIR("staticfiles"))
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = [str(APPS_DIR.path("static"))]
+STATICFILES_DIRS = [str(APPS_DIR.path("static")),
+                    os.path.join(ROOT_DIR, 'node_modules')]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",

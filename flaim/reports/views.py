7afcd5520776232f300fb822e8c3e079fcf9f944
brian <bfssi.brian.carignan@gmail.com>
2020-08-26 17:26:38 -0400

    added new graph to store report

--- flaim/reports/views.py
+++ flaim/reports/views.py
@@ -1,15 +1,15 @@
-import re
+from textwrap import wrap
+from urllib.parse import unquote
 
-import numpy as np
 import pandas as pd
 import plotly.figure_factory as ff
+import plotly.graph_objects as go
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import F
 from django.views.generic import TemplateView
 from plotly.io import to_html
-from urllib.parse import unquote
 
 from flaim.database import models
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class ProductView(LoginRequiredMixin, TemplateView):
@@ -28,24 +28,27 @@ class CategoryView(LoginRequiredMixin, TemplateView):
         if 'category' not in self.kwargs:
             context['category'] = 'Beverages'  # set default category TODO: set this to a random value instead
         else:
-            context['category'] = unquote(
-                self.kwargs['category'])  # pulls category from URL e.g. /reports/category/Beverages
-
-        selection = context['category']
+            # pulls category from URL e.g. /reports/category/Beverages
+            context['category'] = unquote(self.kwargs['category'])
 
         plot_df = get_plot_df()
-        plot_df = plot_df.loc[plot_df['category_text'] == selection]
+        plot_df = plot_df.loc[plot_df['category_text'] == context['category']]
 
         # Top bar
-        context['report_title'] = selection
         context['product_count'] = plot_df.shape[0]
         context['calorie_median'] = f'{plot_df.calories.median():.0f}'
-        context['sodium_median'] = f'{plot_df.sodium_dv.median()*100:.0f}%'
-        context['fat_median'] = f'{plot_df.totalfat_dv.median()*100:.0f}%'
-        context['sugar_median'] = f'{plot_df.sugar.median()*100:.0f}%'
+
+        context['sodium_color'] = get_nutrient_color(plot_df.sodium_dv.median())
+        context['sodium_median'] = f'{plot_df.sodium_dv.median() * 100:.0f}%'
+
+        context['fat_color'] = get_nutrient_color(plot_df.totalfat_dv.median())
+        context['fat_median'] = f'{plot_df.totalfat_dv.median() * 100:.0f}%'
+
+        context['sugar_color'] = get_nutrient_color(plot_df.sugar.median())
+        context['sugar_median'] = f'{plot_df.sugar.median() * 100:.0f}%'
 
         # Visualizations
-        context['figure1'] = get_figure1(plot_df)
+        context['figure1'] = get_nutrient_distribution_plot(plot_df)
         return context
 
 
@@ -64,15 +67,19 @@ class StoreView(LoginRequiredMixin, TemplateView):
         plot_df = plot_df.loc[plot_df['store'] == selection]
 
         # Top bar
-        context['report_title'] = selection
+        context['report_title'] = selection.capitalize()
         context['product_count'] = plot_df.shape[0]
         context['sodium_products_over_15'] = plot_df[plot_df.sodium_dv > 0.15].shape[0]
         context['fat_products_over_15'] = plot_df[plot_df.totalfat_dv > 0.15].shape[0]
         context['sugar_products_over_15'] = plot_df[plot_df.sugar > 0.15].shape[0]
 
         # Visualizations
-        context['figure1'] = get_figure1(plot_df)
+        context['figure1'] = get_category_nutrient_distribution_plot(plot_df)
         return context
+
+
+def get_nutrient_color(value):
+    return 'text-success' if value <= 0.15 else 'text-danger'
 
 
 # Fetch dataset
@@ -91,10 +98,9 @@ def get_plot_df():
 
 
 # Figure generation
-def get_figure1(df):
+def get_nutrient_distribution_plot(df):
     nutrients = ['sodium_dv', 'totalfat_dv', 'sugar']
-    fig = ff.create_distplot(df[nutrients].dropna().T.values,
-                             nutrients, bin_size=.01)
+    fig = ff.create_distplot(df[nutrients].dropna().T.values, nutrients, bin_size=.01)
 
     fig.update_layout(
         width=1100,
@@ -118,4 +124,46 @@ def get_figure1(df):
             color="Red",
             width=2
         )))
+    return to_html(fig, include_plotlyjs=False, full_html=False)
+
+
+def get_category_nutrient_distribution_plot(df):
+    def over_15(row):
+        return 1 if row > 0.15 else 0
+
+    nutrients = ['sodium_dv', 'totalfat_dv', 'sugar']
+    categories = ['Beverages', 'Cereals and Other Grain Products', 'Dairy Products and Substitutes',
+                  'Meat and Poultry, Products and Substitutes', 'Snacks']
+
+    category_labels = ['<br>'.join(wrap(c, 20)) for c in categories]
+
+    plot_df = df.loc[df['category_text'].isin(categories)]
+
+    for n in nutrients:
+        plot_df[f'{n}_count'] = plot_df[n].apply(lambda row: over_15(row))
+
+    plot_df = plot_df[['category_text'] + [f'{n}_count' for n in nutrients]].groupby(
+        'category_text').sum().reindex(categories)
+
+    data = [
+        go.Bar(name=nutrient, x=category_labels, y=plot_df[f'{nutrient}_count']) for nutrient in nutrients
+    ]
+
+    layout = dict(
+        width=1100,
+        margin=dict(
+            l=50,
+            r=20,
+            b=20,
+            t=30,
+        ),
+        xaxis=dict(
+            showgrid=True,
+            tickson='boundaries',
+            tickangle=0,
+        ),
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+
     return to_html(fig, include_plotlyjs=False, full_html=False)

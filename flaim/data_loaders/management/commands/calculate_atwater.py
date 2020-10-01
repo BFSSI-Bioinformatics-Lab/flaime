a995d6be688843fff205a738e798c8e180f8c8b4
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-10-01 14:57:56 -0400

    factored out atwater calculation logic into new method; now only applies to most_recent=True

--- flaim/data_loaders/management/commands/calculate_atwater.py
+++ flaim/data_loaders/management/commands/calculate_atwater.py
@@ -5,18 +5,22 @@ from flaim.database.models import Product, NutritionFacts
 from tqdm import tqdm
 
 
+def calculate_atwater():
+    product_df = pd.DataFrame(list(Product.objects.filter(most_recent=True).values()))
+    nft_df = pd.DataFrame(list(NutritionFacts.objects.filter(product__most_recent=True).values()))
+    df = product_df.merge(nft_df, left_on='id', right_on='product_id')
+    df['atwater_result'] = get_atwater_results(df)
+
+    for i, row in tqdm(df.iterrows(), total=len(df), desc="Updating database"):
+        product = Product.objects.get(id=row['id_x'])
+        product.atwater_result = row['atwater_result']
+        product.save()
+
+
 class Command(BaseCommand):
-    help = 'This script will run the Atwater calculation on all products in the database'
+    help = 'This script will run the Atwater calculation on the most recent products in the database'
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS(f'Calculating Atwater results...'))
-        product_df = pd.DataFrame(list(Product.objects.all().values()))
-        nft_df = pd.DataFrame(list(NutritionFacts.objects.all().values()))
-        df = product_df.merge(nft_df, left_on='id', right_on='product_id')
-        df['atwater_result'] = get_atwater_results(df)
-
-        for i, row in tqdm(df.iterrows(), total=len(df), desc="Updating database"):
-            product = Product.objects.get(id=row['id_x'])
-            product.atwater_result = row['atwater_result']
-            product.save()
+        calculate_atwater()
         self.stdout.write(self.style.SUCCESS(f'Done!'))

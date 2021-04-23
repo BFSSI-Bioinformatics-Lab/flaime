48a558a95b9d27523af57e71f62da5fe1a079c46
Forest Dussault <forest@flaime-master.scdc.dio.re>
2021-04-23 14:42:36 +0000

    fixed up the export_product_report

--- flaim/reports/management/commands/export_product_report.py
+++ flaim/reports/management/commands/export_product_report.py
@@ -2,7 +2,7 @@ import pandas as pd
 from django.core.management.base import BaseCommand
 from django.db.models import Q
 from flaim.database.product_mappings import REFERENCE_CATEGORIES_DICT, PRODUCT_STORES
-from flaim.database.models import Product, NutritionFacts
+from flaim.database.models import Product, NutritionFacts, ReferenceCategorySupport
 from pathlib import Path
 from typing import Optional
 from datetime import datetime
@@ -13,6 +13,7 @@ def get_report(most_recent: bool, categories: Optional[list], stores: Optional[l
     products = Product.objects.all()
     products = products.filter(most_recent=most_recent)
     nutrition = NutritionFacts.objects.filter(product__most_recent=most_recent)
+    ref_categories = ReferenceCategorySupport.objects.all()
 
     # Filter queryset by categories
     if categories is not None:
@@ -41,8 +42,18 @@ def get_report(most_recent: bool, categories: Optional[list], stores: Optional[l
     products = products.annotate(manual_category=F('category__manual_category'))
     products = products.annotate(manual_subcategory=F('subcategory__manual_subcategory'))
 
-    product_df = pd.DataFrame(list(products.values()))
-    nft_df = pd.DataFrame(list(nutrition.values()))
+    product_df = pd.DataFrame(list(products.values())).drop(columns=['category_id','subcategory_id']) # These we want to add, but we want them for the automatic AND the manual
+    nft_df = pd.DataFrame(list(nutrition.values())).drop(columns=['id','created','modified']) # These will have a merge artifact onto the original tables
+    categories_df = pd.DataFrame(list(ref_categories.values()))
+
+    # Add the cateogry and subcategory id for both the manual and predicted
+    product_df = product_df.merge(categories_df[["category_id","category_name"]].drop_duplicates(), how = "left", left_on = "predicted_category", right_on = "category_name").rename(columns={"category_id":"predicted_category_id"}).drop(columns="category_name") 
+    product_df = product_df.merge(categories_df[["subcategory_id","subcategory_name"]].drop_duplicates(), how = "left", left_on = "predicted_subcategory", right_on = "subcategory_name").rename(columns={"subcategory_id":"predicted_subcategory_id"}).drop(columns="subcategory_name")
+    product_df = product_df.merge(categories_df[["category_id","category_name"]].drop_duplicates(), how = "left", left_on = "manual_category", right_on = "category_name").rename(columns={"category_id":"manual_category_id"}).drop(columns="category_name")
+    product_df = product_df.merge(categories_df[["subcategory_id","subcategory_name"]].drop_duplicates(), how = "left", left_on = "manual_subcategory", right_on = "subcategory_name").rename(columns={"subcategory_id":"manual_subcategory_id"}).drop(columns="subcategory_name")
+    
+    
+
     df = product_df.merge(nft_df, left_on='id', right_on='product_id')
     return df
 

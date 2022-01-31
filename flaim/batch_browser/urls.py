84396012fb8b16a1dff6b02efb58df5e115991ce
stephanie <bfssi.stephanie.diaz@gmail.com>
2022-01-31 09:14:22 -0500

    added  pdf func to batch_browser vies, url and html added

--- flaim/batch_browser/urls.py
+++ flaim/batch_browser/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, re_path
 
 from flaim.batch_browser.views import IndexView, BatchView
+from flaim.batch_browser.views import render_pdf_page
 
 app_name = "batch_browser"
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     re_path("^(?P<pk>\d+)$", view=BatchView.as_view(), name="batch_view"),
+    path('pdf/', render_pdf_page, name='test-pdf')
 ]
--- flaim/batch_browser/views.py
+++ flaim/batch_browser/views.py
@@ -1,6 +1,14 @@
 import logging
 from flaim.database import models
 from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404
+
+import os
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from django.contrib.staticfiles import finders
 
 logger = logging.getLogger(__name__)
 
@@ -24,3 +32,48 @@ class BatchView(DetailView):
         context = super().get_context_data(**kwargs)
 
         return context
+
+
+def render_pdf_page(request, *args, **kwargs):
+    scrapeBatch = models.ScrapeBatch
+    template_path = 'batch_browser/pdf-view.html'
+    context = {'scrapeBatch': scrapeBatch}
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    # if download wanted:
+    # response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    # render pdf in page:
+    response['Content-Disposition'] = 'filename="report.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+        html, dest=response)
+    # if error then show some funny view
+    if pisa_status.err:
+        return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
+
+# def render_pdf_page(request):
+#     scrapeBatch = models.ScrapeBatch
+#     template_path = 'batch_browser/pdf-view.html'
+#     context = {'scrapeBatch': scrapeBatch}
+#     # Create a Django response object, and specify content_type as pdf
+#     response = HttpResponse(content_type='application/pdf')
+#     # if download wanted:
+#     # response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+#     # render pdf in page:
+#     response['Content-Disposition'] = 'filename="report.pdf"'
+#     # find the template and render it.
+#     template = get_template(template_path)
+#     html = template.render(context)
+#
+#     # create a pdf
+#     pisa_status = pisa.CreatePDF(
+#         html, dest=response)
+#     # if error then show some funny view
+#     if pisa_status.err:
+#         return HttpResponse('We had some errors <pre>' + html + '</pre>')
+#     return response
